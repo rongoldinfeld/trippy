306ef726e0d609e8af55d501d05a83603d6d1c65
Ron Goldinfeld <rongoldinfeld56@gmail.com>
2021-02-02 17:04:45 +0200

    rename AddTripFragment to AddTripButtonFragment

--- app/src/main/java/com/colman/trippy/AddTripButtonFragment.java
+++ app/src/main/java/com/colman/trippy/AddTripButtonFragment.java
@@ -8,13 +8,13 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-public class AddTripFragment extends Fragment {
+public class AddTripButtonFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View fragmentContainer =  inflater.inflate(R.layout.fragment_add_trip, container, false);
+        View fragmentContainer =  inflater.inflate(R.layout.fragment_add_trip_button, container, false);
         fragmentContainer.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_userProfile_to_createTrip));
 
         return fragmentContainer;
